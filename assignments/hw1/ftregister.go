0da08a927906ae2776b57565c39836c8abdd3ee3
Vadim <varem99@mail.ru>
2020-11-03 13:22:55 +0300

    read and write

--- assignments/hw1/ftregister.go
+++ assignments/hw1/ftregister.go
@@ -1,19 +1,80 @@
 package ftregister
 
 import (
+	"fmt"
 	"github.com/dati-mipt/distributed-storage-algorithms/network"
 	"github.com/dati-mipt/distributed-storage-algorithms/util"
 )
 
 type FaultTolerantRegister struct {
-	rid int64
-	current util.TimestampedValue
+	rid      int64
+	current  util.TimestampedValue
 	replicas map[int64]network.Link
 }
 
-func NewFaultTolerantRegister(rid int64) *FaultTolerantRegister{
+func NewFaultTolerantRegister(rid int64) *FaultTolerantRegister {
 	return &FaultTolerantRegister{
 		rid:      rid,
 		replicas: map[int64]network.Link{},
+	}
+}
+
+func (r *FaultTolerantRegister) Write(value int64) bool {
+	fmt.Printf("write value: %d\n", value)
+	
+	r.current.Val = value
+	r.current.Ts = util.Timestamp{Number: r.current.Ts.Number + 1, Rid: r.rid}
+
+
+	return true
+}
+
+// compound read = {read + write}
+
+func (r *FaultTolerantRegister) Read() int64 {
+	fmt.Printf("rid: %d, ts: %d, read value: %d\n", r.current.Ts.Number, r.rid, r.current.Val)
+
+	// read data from replica and set current TimestampedValue due to max replica id
+
+	var msg interface{}
+	if update, ok := msg.(util.TimestampedValue); ok {
+		if r.current.Ts.Less(update.Ts) {
+			r.current = update
+		}
+	}
+
+	// update Timestamp
+
+	r.current.Ts = util.Timestamp{Number: r.current.Ts.Number + 1, Rid: r.rid}
+
+	// write value to replicas
+
+	for _, rep := range r.replicas {
+		rep.AsyncMessage(r.current)
+	}
+
+	return r.current.Val
+}
+
+func (r *FaultTolerantRegister) Introduce(rid int64, link network.Link) {
+	if link != nil {
+		r.replicas[rid] = link
+	}
+
+}
+
+// make a copy of data
+func (r *FaultTolerantRegister) Receive(rid int64, msg interface{}) interface{} {
+	if update, ok := msg.(util.TimestampedValue); ok {
+		if r.current.Ts.Less(update.Ts) {
+			r.current = update
+		}
+	}
+	return nil
+}
+
+func (r *FaultTolerantRegister) Update() {
+	for _, rep := range r.replicas {
+		rep.AsyncMessage(r.current)
 	}
 }
--- assignments/hw1/register_test.go
+++ assignments/hw1/register_test.go
@@ -1,1 +1,89 @@
 package ftregister
+
+import (
+	"fmt"
+	"testing"
+	"time"
+	"github.com/dati-mipt/distributed-storage-algorithms/network"
+)
+
+type mockRegister struct {
+	current int64
+}
+
+func (r *mockRegister) Write(value int64) bool {
+	r.current = value
+	return true
+}
+
+func (r *mockRegister) Read() int64 {
+	return r.current
+}
+
+func TestGenericOperations(t *testing.T) {
+	var mock = mockRegister{}
+	var fault_tolerant = NewFaultTolerantRegister(0)
+
+	var faultTolerantRegisterCheck = func(reg Register) error {
+		var expected int64 = 5
+		reg.Write(expected)
+
+		var read = reg.Read()
+		if read != expected {
+			return fmt.Errorf("wrong register value, got: %v, expected %v", read, expected)
+		}
+
+		return nil
+	}
+
+	for _, c := range []Register{&mock, fault_tolerant} {
+		if err := faultTolerantRegisterCheck(c); err != nil {
+			t.Errorf("failed fault tolerant API test for %T: %v", c, err)
+		}
+	}
+}
+
+func TestFaultTolerantRegisterReplicaSet(t *testing.T) {
+	var n = network.NewReliableNetwork()
+
+	var regs []*FaultTolerantRegister
+	for i := int64(0); i < 2; i++ {
+		var reg = NewFaultTolerantRegister(i)
+		n.Register(i, reg)
+		regs = append(regs, reg)
+	}
+
+	if regs == nil {
+		t.Error("syntax check failed")
+		return
+	}
+
+	go n.Route()
+
+	if regs[0].Read() != 0 {
+		t.Errorf("wrong read value, got: %d, expected %d", regs[0].Read(), 0)
+		return
+	}
+
+	regs[0].Write(4)
+	regs[0].Update()
+
+	time.Sleep(time.Millisecond)
+
+	if regs[1].Read() != 4 {
+		t.Errorf("wrong read value, got: %d, expected %d", regs[1].Read(), 4)
+		return
+	}
+
+	time.Sleep(time.Millisecond)
+
+	regs[0].Write(8)
+	regs[0].Update()
+
+	time.Sleep(time.Millisecond)
+
+	if regs[1].Read() != 8 {
+		t.Errorf("wrong read value, got: %d, expected %d", regs[1].Read(), 4)
+		return
+	}
+}
