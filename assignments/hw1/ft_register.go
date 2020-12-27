2d6b350a09a8857eba07164c7151782c3026344e
Ilya Kompaniets <ilya@kompaniets.dev>
2020-12-27 15:35:22 +0300

    Added tests provided by Phikimon

---
+++ assignments/hw1/ft_register.go
@@ -0,0 +1,27 @@
+package hw1
+
+import (
+	"github.com/dati-mipt/distributed-storage-algorithms/network"
+)
+
+type FaultTolerantRegister struct {
+}
+
+func NewFaultTolerantRegister(rid int64) *FaultTolerantRegister {
+	return &FaultTolerantRegister{}
+}
+
+func (r *FaultTolerantRegister) Write(value int64) bool {
+	return false
+}
+
+func (r *FaultTolerantRegister) Read() int64 {
+	return 0
+}
+
+func (r *FaultTolerantRegister) Introduce(rid int64, link network.Link) {
+}
+
+func (r *FaultTolerantRegister) Receive(rid int64, msg interface{}) interface{} {
+	return nil
+}
---
+++ assignments/hw1/ft_register_test.go
@@ -0,0 +1,100 @@
+package hw1
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+
+	"github.com/dati-mipt/distributed-storage-algorithms/network"
+)
+
+func TestFaultTolerantRegister(t *testing.T) {
+	var n = network.NewReliableNetwork()
+
+	var regs []*FaultTolerantRegister
+	for i := int64(0); i < 2; i++ {
+		var reg = NewFaultTolerantRegister(i)
+		n.Register(i, reg)
+		regs = append(regs, reg)
+	}
+
+	if regs == nil {
+		t.Error("syntax check failed")
+		return
+	}
+
+	go n.Route()
+
+	if regs[0].Read() != 0 {
+		t.Errorf("wrong read value regs[%d], got: %d, expected %d", 0, regs[0].Read(), 0)
+		return
+	}
+
+	n.Wait()
+	regs[0].Write(4)
+
+	n.Wait()
+	if regs[0].Read() != 4 {
+		t.Errorf("wrong read value regs[%d], got: %d, expected %d", 0, regs[0].Read(), 4)
+		return
+	}
+
+	n.Wait()
+	if regs[1].Read() != 4 {
+		t.Errorf("wrong read value regs[%d], got: %d, expected %d", 1, regs[1].Read(), 4)
+		return
+	}
+}
+
+func TestFaultTolerantRegisterDieHard(t *testing.T) {
+	var n = network.NewReliableNetwork()
+
+	var clusterSize = 30
+	var iterationsNumber = clusterSize * 5
+
+	var regs []*FaultTolerantRegister
+	for i := int64(0); i < int64(clusterSize); i++ {
+		var reg = NewFaultTolerantRegister(i)
+		n.Register(i, reg)
+		regs = append(regs, reg)
+	}
+
+	if regs == nil {
+		t.Error("syntax check failed")
+		return
+	}
+
+	go n.Route()
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		for i := 0; i < iterationsNumber; i++ {
+			num := (rand.Int() / 2) % clusterSize
+			regs[num].Write(int64(i))
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for i := 0; i < iterationsNumber; i++ {
+			num := rand.Int() % clusterSize
+			val := regs[num].Read()
+			if val >= int64(iterationsNumber) || val < 0 {
+				t.Errorf("wrong read value regs[%d], got: %d, max expected %d", num, val, int64(iterationsNumber-1))
+				return
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	n.Wait()
+
+	if regs[0].Read() != int64(iterationsNumber-1) {
+		t.Errorf("wrong read value regs[%d], got: %d, expected %d", 0, regs[0].Read(), int64(iterationsNumber-1))
+		return
+	}
+}
