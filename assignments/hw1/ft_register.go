73fa01662fa8e991c54fb5f821ff0d529371ee86
crendelyok <nikita.pankov.v@gmail.com>
2021-11-23 04:31:01 +0300

    attempt_1

--- assignments/hw1/ft_register.go
+++ assignments/hw1/ft_register.go
@@ -3,6 +3,7 @@ package hw1
 import (
 	"github.com/dati-mipt/distributed-systems/network"
 	"github.com/dati-mipt/distributed-systems/util"
+	"context"
 )
 
 type FaultTolerantRegister struct {
@@ -18,14 +19,50 @@ func NewFaultTolerantRegister(rid int64) *FaultTolerantRegister {
 	}
 }
 
+func (r *FaultTolerantRegister) ReadQuorum() bool {
+	i := 0
+	for _, l := range r.replicas {
+		if msg, ok := (<-l.Send(context.Background(), struct{}{})).(util.TimestampedValue); ok{
+			r.current.Store(msg)
+			i++
+		}
+	}
+	if (i > len(r.replicas) / 2) {
+		return true
+	}
+	return false
+}
+
+func (r *FaultTolerantRegister) WriteQuorum() bool {
+	i := 0
+	for _, l := range r.replicas {
+		if _, ok := (<-l.Send(context.Background(), r.current)).(util.TimestampedValue); ok{
+			i++
+		}
+	}
+	if (i > len(r.replicas) / 2) {
+		return true
+	}
+	return false
+}
+
 func (r *FaultTolerantRegister) Write(value int64) bool {
+	if !r.ReadQuorum() {
+		return false
+	}
 	r.current.Val = value
 	r.current.Ts  = util.Timestamp{Number: r.current.Ts.Number + 1, Rid: r.rid}
+	if !r.WriteQuorum() {
+		return false
+	}
 	return true
 }
 
 func (r *FaultTolerantRegister) Read() int64 {
-	return r.current.Val
+	if (r.ReadQuorum() && r.WriteQuorum()) {
+		return r.current.Val
+	}
+	return 404 //?????
 }
 
 func (r *FaultTolerantRegister) Introduce(rid int64, link network.Link) {
@@ -35,11 +72,13 @@ func (r *FaultTolerantRegister) Introduce(rid int64, link network.Link) {
 }
 
 func (r *FaultTolerantRegister) Receive(rid int64, msg interface{}) interface{} {
-	print("receive ", rid, "\n")
-	if update, ok := msg.(util.TimestampedValue); ok {
-		if r.current.Ts.Less(update.Ts) {
-			r.current = update
-		}
+	switch t:= msg.(type) {
+	case util.TimestampedValue: {
+		r.current.Store(t)
+		return r.current
+	}
+	case  struct{}:
+		return r.current
 	}
 	return nil
 }
