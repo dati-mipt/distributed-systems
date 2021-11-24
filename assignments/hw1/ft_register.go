c13f250ace663b75ca0218dd48a7fcd8ca58a66d
Oleg Shitikov <shitikov.om@phystech.edu>
2021-11-24 14:50:45 +0300

    Quorum interaction changed

--- assignments/hw1/ft_register.go
+++ assignments/hw1/ft_register.go
@@ -2,6 +2,7 @@ package hw1
 
 import (
 	"context"
+	"fmt"
 	"github.com/dati-mipt/distributed-systems/network"
 	"github.com/dati-mipt/distributed-systems/util"
 )
@@ -9,12 +10,12 @@ import (
 type FaultTolerantRegister struct {
 	rid      int64
 	current  util.TimestampedValue
-	replicas map[int64]network.Link
+	replicas []network.Link
 }
 
 func NewFaultTolerantRegister(rid int64) *FaultTolerantRegister {
 	return &FaultTolerantRegister{
-		replicas: map[int64]network.Link{},
+		replicas: []network.Link{},
 		current: util.TimestampedValue{Ts: util.Timestamp{Rid: rid}},
 	}
 }
@@ -43,7 +44,7 @@ func (r *FaultTolerantRegister) Read() int64 {
 }
 
 func (r *FaultTolerantRegister) Introduce(rid int64, link network.Link) {
-	r.replicas[rid] = link
+	r.replicas = append(r.replicas, link)
 }
 
 func (r *FaultTolerantRegister) Receive(rid int64, msg interface{}) interface{} {
@@ -58,34 +59,68 @@ func (r *FaultTolerantRegister) Receive(rid int64, msg interface{}) interface{} 
 }
 
 func (r *FaultTolerantRegister) quorumCount() int {
-	return len(r.replicas) / 2 + 1
+	return (len(r.replicas) + 1) / 2 + 1
 }
 
 // update current value in node r with values in quorum
 func (r *FaultTolerantRegister) updateFromQuorum() bool {
-	successCnt := 1
-	for _, link := range r.replicas {
-		if msg, ok := (<-link.Send(context.Background(), struct{}{})).(util.TimestampedValue); ok {
-			r.current.Store(msg)
-			successCnt++
-			if successCnt >= r.quorumCount() {
-				break
+	return r.useQuorum(
+		struct{}{},
+		func(msg interface{}) bool {
+			if msg, ok := (msg).(util.TimestampedValue); ok {
+				r.current.Store(msg)
+				return true
 			}
-		}
-	}
-	return successCnt >= r.quorumCount()
+			return false
+		})
 }
 
 // update values in quorum nodes with current node value
 func (r *FaultTolerantRegister) updateQuorum() bool {
-	successCnt := 1
-	for _, link := range r.replicas {
-		if _, ok := (<-link.Send(context.Background(), r.current)).(bool); ok {
-			successCnt++
-			if successCnt >= r.quorumCount() {
-				break
+	return r.useQuorum(
+		r.current,
+		func(msg interface{}) bool {
+			if _, ok := (msg).(bool); ok {
+				return true
 			}
+			return false
+		})
+}
+
+// send msg to quorum and call callback on responses
+func (r *FaultTolerantRegister) useQuorum(msg interface{}, callback func(msg interface{}) bool) bool {
+	successCnt := 1
+	curLink := 0
+
+	responses := make(chan interface{})
+
+	updateFunc := func(linkId int) {
+		responses<-<-r.replicas[linkId].Send(context.Background(), msg)
+	}
+
+	for ; curLink < r.quorumCount() - 1; curLink++ {
+		go updateFunc(curLink)
+	}
+
+	for successCnt < r.quorumCount() && curLink < len(r.replicas) {
+		if callback(<-responses) {
+			successCnt++
+		} else {
+			go updateFunc(curLink);
+			curLink++
 		}
 	}
+
+	for i := 0; i < r.quorumCount() - successCnt; i++ {
+		if callback(<-responses) {
+			successCnt++
+		}
+	}
+
 	return successCnt >= r.quorumCount()
 }
+
+func (r *FaultTolerantRegister) print() (s string) {
+	return fmt.Sprintf("%d %d %d", r.rid, r.current.Ts.Number, r.current.Val)
+}
+
